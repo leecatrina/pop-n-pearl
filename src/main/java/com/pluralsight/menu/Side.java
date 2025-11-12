a7211071f7f10d5385a163a774c4931c42076452
leecatrina <cninalee3@gmail.com>
2025-11-12 15:48:44 -0500

    add Side

--- src/main/java/com/pluralsight/menu/Side.java
+++ src/main/java/com/pluralsight/menu/Side.java
@@ -1,4 +1,37 @@
 package com.pluralsight.menu;
 
-public class Side {
+public class Side implements MenuItem {
+   private String name;
+   private double price;
+
+    public Side(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    //implement MenuItem methods
+    @Override
+    public String getName() {
+        return "";
+    }
+
+    @Override
+    public double calculatePrice() {
+        return price;
+    }
+    // to string
+    public String toString() {
+        return name + " ($" + price + ")";
+    }
 }
