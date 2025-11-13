00fabf239571d25983806d8329e21fc4ada90106
leecatrina <cninalee3@gmail.com>
2025-11-12 22:43:11 -0500

    add Order, add Receipt

--- src/main/java/com/pluralsight/order/Order.java
+++ src/main/java/com/pluralsight/order/Order.java
@@ -1,4 +1,34 @@
 package com.pluralsight.order;
 
+import com.pluralsight.menu.MenuItem;
+
+import java.util.List;
+
 public class Order {
+    private List<MenuItem> items; //stores all items in this order
+
+    //constructor
+    public Order(List<MenuItem> items) {
+        this.items = items;
+    }
+    //Add a MenuItem (MilkTea, Drink, Side, etc)
+    public void addItem(MenuItem item) {
+        items.add(item);
+    }
+    //Remove an item from the order
+    public void removeItem(MenuItem item){
+        items.remove(item);
+    }
+    //Get the list of items
+    public List<MenuItem>getItems(){
+        return items;
+    }
+    //Calculate the total price of all items
+    public double calculateTotal() {
+        double total = 0.0;
+        for (MenuItem item : items) {
+            total += item.calculatePrice();
+        }
+        return total;
+    }
 }
--- src/main/java/com/pluralsight/order/Receipt.java
+++ src/main/java/com/pluralsight/order/Receipt.java
@@ -1,4 +1,24 @@
 package com.pluralsight.order;
 
+import com.pluralsight.menu.MenuItem;
+
+import java.text.DecimalFormat;
+
 public class Receipt {
+    private Order order;
+
+    public Receipt(Order order){
+        this.order = order;
+    }
+    public void printReceipt(){
+        DecimalFormat df = new DecimalFormat("#.00");
+        System.out.println("========== RECEIPT ==========");
+
+        for(MenuItem item: order.getItems()){
+            System.out.println(item.getName() + " - $" + df.format(item.calculatePrice()));
+        }
+        System.out.println("-----------------------------");
+        System.out.println("TOTAL: $" + df.format(order.calculateTotal()));
+        System.out.println("=============================");
+    }
 }
